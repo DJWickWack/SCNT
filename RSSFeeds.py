53a17c5c594791aa7d60ab8662ed6fe7516ede0b
DJWickWack <35749854+DJWickWack@users.noreply.github.com>
2020-06-09 11:49:44 -0400

    added RSSFeeds.py that can pull articles from a URL

---
+++ RSSFeeds.py
@@ -0,0 +1,25 @@
+import feedparser
+
+
+class RSSFeeds():
+
+    def GetFeed(self, feedurl):
+        return  feedparser.parse(feedurl)
+
+    def GetArticles(self, feed):
+        arts=[]
+        entries = feed['entries']
+        for x in entries:
+            arts.append({
+                'id': x['id'],
+                'title': x['title'],
+                'summary': x['summary'],
+                'date': x['published_parsed']
+            })
+        return arts
+
+
+
+d=RSSFeeds()
+f=d.GetFeed('https://cointelegraph.com/rss/tag/bitcoin')
+print(d.GetArticles(f))
