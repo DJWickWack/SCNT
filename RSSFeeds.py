232c0b631346e78b1a6bd15136d2a42d5ec94186
Andrew Bissel <andrew@bissel.dev>
2020-06-22 03:07:16 -0400

    parse HTML to get text found in articles

--- RSSFeeds.py
+++ RSSFeeds.py
@@ -6,6 +6,16 @@ class RSSFeeds():
     def GetFeed(self, feedurl):
         return feedparser.parse(feedurl)
 
+    '''
+    Input: Parsed RSS feed
+    Output: List of articles. Each article is a dictoinary containing the following data:
+        'id': url to article
+        'title': title of article
+        'summary': summary of article
+        'date': date article published
+        'texts': all strings of text found inside <p></p>
+        
+    '''
     def GetArticles(self, feed):
         arts = []
         entries = feed['entries']
@@ -16,6 +26,17 @@ class RSSFeeds():
                 'summary': x['summary'],
                 'date': x['published_parsed']
             })
+
+        # Add entry to articles, contains list of paragraph texts to analyze
+        # todo: generating duplicates for me, but works for now.
+        for article in arts:
+            soup = bs4.BeautifulSoup(article['summary'])
+            paragraphs = soup.find_all('p')
+            text_found = []
+            for p in paragraphs:
+                text_found.append(p.text)
+            article['texts'] = text_found
+
         return arts
 
 
