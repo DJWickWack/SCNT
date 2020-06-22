8fb63f667eb454db5937a8fee913a24f29a1a5aa
Andrew <7138697+andybiss@users.noreply.github.com>
2020-06-22 03:09:12 -0400

    7 rss html parse (#12)
    * gitignore to ignore virtual environments
    
    * add requirements.txt
    
    * beautifulsoup add
    
    * beautifulsoup add
    
    * parse HTML to get text found in articles
    
    Co-authored-by: Andrew Bissel <andrew@bissel.dev>

--- RSSFeeds.py
+++ RSSFeeds.py
@@ -1,13 +1,23 @@
 import feedparser
-
+import bs4
 
 class RSSFeeds():
 
     def GetFeed(self, feedurl):
-        return  feedparser.parse(feedurl)
+        return feedparser.parse(feedurl)
 
+    '''
+    Input: Parsed RSS feed
+    Output: List of articles. Each article is a dictoinary containing the following data:
+        'id': url to article
+        'title': title of article
+        'summary': summary of article
+        'date': date article published
+        'texts': all strings of text found inside <p></p>
+        
+    '''
     def GetArticles(self, feed):
-        arts=[]
+        arts = []
         entries = feed['entries']
         for x in entries:
             arts.append({
@@ -16,10 +26,21 @@ class RSSFeeds():
                 'summary': x['summary'],
                 'date': x['published_parsed']
             })
+
+        # Add entry to articles, contains list of paragraph texts to analyze
+        # todo: generating duplicates for me, but works for now.
+        for article in arts:
+            soup = bs4.BeautifulSoup(article['summary'])
+            paragraphs = soup.find_all('p')
+            text_found = []
+            for p in paragraphs:
+                text_found.append(p.text)
+            article['texts'] = text_found
+
         return arts
 
 
 # for testing
-# d=RSSFeeds()
-# f=d.GetFeed('https://cointelegraph.com/rss/tag/bitcoin')
-# print(d.GetArticles(f))
+d = RSSFeeds()
+f = d.GetFeed('https://cointelegraph.com/rss/tag/bitcoin')
+print(d.GetArticles(f)[0]['summary'])
