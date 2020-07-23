2bc15b08557daebd95838dd645115d1e328213c6
DJWickWack <35749854+DJWickWack@users.noreply.github.com>
2020-07-22 22:55:41 -0400

    tweet error

--- Tweets.py
+++ Tweets.py
@@ -18,22 +18,19 @@ class Tweets():
             A.append(st.full_text)
         return A
 
-    def Past10(self, hashtag, amount, sincedate, untildate):
-        if (sincedate==None or untildate ==None):
-            today = datetime.datetime.now()
-            limit = datetime.datetime(today.year,today.month,today.day-10)
-            self.Past10(hashtag,today,limit,amount)
-        day = sincedate
-        print(day)
-        nextday= day + datetime.timedelta(days=1)
+    def Past10(self, hashtag, amount):
+
+
+        limit = datetime.datetime.now() - datetime.timedelta(days=10)
         A = []
-        twt = self.api.search(q=hashtag, count=amount, since= day,until= day, tweet_mode='extended', lang='en')
-        for st in twt:
-            A.append(st.full_text)
-        if (day == untildate):
-            return A
-        if (day != untildate):
-            A = A + self.Past10(hashtag,amount,nextday,untildate)
+        for x in range(10):
+            twt = self.api.search(q=hashtag, count=amount, since=limit, until=limit, tweet_mode='extended', lang='en')
+            for y in twt:
+                A.append(y.full_text)
+            limit += datetime.timedelta(days=1)
+
+        return A
+
 
 
 
@@ -42,7 +39,7 @@ class Tweets():
 # startDate = datetime.datetime(2020, 7, 1, 0, 0, 0)
 # endDate = datetime.datetime(2020, 1, 1, 0, 0, 0)
 rt = Tweets()
-
-a = rt.Past10('#Bitcoin',1,None,None)
+a= rt.HashTweet("#bitcoin",10)
+# a = rt.Past10('#Bitcoin',1)
 print(a)
 
