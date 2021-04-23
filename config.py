04f9415a5ec856072ff19019774a217897f3a064
DJWickWack <35749854+DJWickWack@users.noreply.github.com>
2021-04-23 00:17:13 -0400

    shhh keys
    keys where visable

--- config.py
+++
@@ -1,7 +0,0 @@
-class key():
-    consumer_key = "WTlR4vdaXPAx15eRKzarFFK5M"
-    consumer_secret = "V3stPYHjdQVCKEZE8dqzZNOzNJndaoX2rcI4pRAf090dBmLrf6"
-
-    access_token = "1041353482794500097-vpTMPwSQuwjui2mHwsK22VqjemboBW"
-    access_token_secret = "bJYM9LREpj2q4BU9yT1zKVGM7BXpf8N0ao3TfBSBg6e5M"
-    b = 1
